fc92f1e972ca9acfdd77c3e73c4e74819bdddcc1
Kemppainen Ville (FMI) <Ville.Kemppainen@fmi.fi>
2023-05-05 08:45:18 +0300

    More tests

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-require('dotenv').config()
-const express = require('express')
-require('express-async-errors');
-const app = express()
-const middleware = require('./utils/middleware')
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const loginRouter = require('./controllers/loginRouter')
-const blogsRouter = require('./controllers/blogsRouter')
-const usersRouter = require('./controllers/usersRouter')
-
-mongoose.connect(process.env.MONGO_URL)
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/login', loginRouter)
-app.use('/api/blogs', middleware.userExtractor, middleware.tokenExtractor, blogsRouter)
-app.use('/api/users', middleware.tokenExtractor, usersRouter)
-
-app.use(middleware.errorHandler)
+const app = require('./app')
 
 const PORT = 3003
 app.listen(PORT, () => {
